5ac79339e166504ec15fefc13a3959bbdd9cbe53
Yan Julião <yanjuliao7@gmail.com>
2023-01-26 17:05:41 -0300

    Implementação da Home Page

--- setup/settings.py
+++ setup/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'clientes',
+    'home',
 ]
 
 MIDDLEWARE = [
--- setup/urls.py
+++ setup/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from clientes import urls as clientes_urls
+from home import urls as home_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    path('', include(home_urls)),
     path('clientes/', include(clientes_urls)),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
